86379c13ba289acbaacb3748ae08d308276afac6
jddelia <jddelia123@gmail.com>
2020-03-31 22:32:05 -0400

    Add How It Works section

--- src/components/Home/HowItWorks/Steps.js
+++ src/components/Home/HowItWorks/Steps.js
@@ -13,7 +13,12 @@ function Steps() {
         </div>
 
         <div className="stepSectionContent">
-
+          <p className="stepContentText">
+            <span className="stepAccent">Search</span> <br/> 
+            Search for a quote. <br/>
+            Filter your search by theme, and content, until you find
+            one which you'd like to make the theme for your day. 
+          </p>
         </div>
       </div>
 
@@ -27,7 +32,13 @@ function Steps() {
         </div>
 
         <div className="stepSectionContent">
-
+        <p className="stepContentText">
+            <span className="stepAccent">Select</span> <br/>
+            Select a quote. <br/>
+            Once you find a quote, save it and select frequency,
+            so you can use it
+            for motivation. 
+          </p>
         </div>
       </div>
 
@@ -41,7 +52,13 @@ function Steps() {
         </div>
 
         <div className="stepSectionContent">
-
+          <p className="stepContentText">
+            <span className="stepAccent">Refocus</span> <br/>
+            Refocus your mind. <br/>
+            Use the selected quote, to refocus your mind throughout
+            the day. Set specific intervals, to recieve reminders, so
+            you won't lose focus. 
+          </p>
         </div>
       </div>
     </div>
