d1a3047d8fbc45e76c15e01081b249a1bf5fdf7e
jddelia <jddelia123@gmail.com>
2020-04-29 19:41:25 -0400

    Update notification icon

--- src/firebase/postNotification.js
+++ src/firebase/postNotification.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const key = "AAAAHBm5F1g:APA91bF7PLVRe-W0xr2Jtc3WLSkdI9IM3_MdNMdPKKurQlMHkHG0HrX6uhbCICFAAPjQ89ccnJFUt9zzWKQ89qEcza7SBFDLFWryQKnhN4D8zbhO__OxFQPb2F7785pEjV5M3bCNhKW8";
 const postURL = "https://fcm.googleapis.com/fcm/send";
 const mindRightMainLogo = "https://i.imgur.com/yrDnY9e.png";
+const notificationBadge = "https://i.imgur.com/GY9lfvL.png";
 
 function createNotification(
   {
@@ -14,9 +15,10 @@ function createNotification(
     "collapse_key" : "type_a",
     "notification" : {
       "title": "MINDRIGHT",
-      "body" : `${notificationBody}`,
+      "body": `${notificationBody}`,
       "icon": mindRightMainLogo,
-      "badge": mindRightMainLogo
+      "badge": notificationBadge,
+      "vibrate": [1,2,3]
     },
     "to": `${userToken}`
   }
