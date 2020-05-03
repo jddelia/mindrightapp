f7da525ef7b38fc9476c435710a6565679667b7f
jddelia <jddelia123@gmail.com>
2020-05-02 22:49:49 -0400

    Update about content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,7 +13,7 @@ function About() {
           The user selects from a series of "quote cards",
           which all contain motivational content. Then the user
           selects a card(s) which they find motivating and focus
-          themselves on behaving in the spirit on their selected
+          themselves on behaving in the spirit of their selected
           card(s). Timed notifications also allow the user to refocus
           on the card(s).
         </p>
